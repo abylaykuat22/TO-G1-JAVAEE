a2cc98697a37d8b8617b01690b18ad2cc840b406
abylaykuat22 <abylaykuat@gmail.com>
2024-02-01 20:55:02 +0600

    JSP

---
+++ src/servlets/HomeServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import db.DbManager;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import models.Post;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Post post = new Post();
+        post.setTitle("ASKJDHGSADSAHDG");
+        post.setCategory(Post.CategoryType.SPORT);
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
---
+++ web/home.jsp
@@ -0,0 +1,18 @@
+<%@ page import="java.io.PrintWriter" %>
+<%@ page import="java.util.List" %><%--
+  Created by IntelliJ IDEA.
+  User: Kuat
+  Date: 01.02.2024
+  Time: 19:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <%@include file="head.jsp"%>
+</head>
+<body>
+<%@include file="navbar.jsp"%>
+<h1>HOME PAGE</h1>
+</body>
+</html>
