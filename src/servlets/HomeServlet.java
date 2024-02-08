95b36735d1bae8c3e2f71d1a0e17004d884f9cd0
abylaykuat22 <abylaykuat@gmail.com>
2024-02-08 20:48:49 +0600

    JDBC CRUDS

--- src/servlets/HomeServlet.java
+++ src/servlets/HomeServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.Item;
 import models.Post;
 
 import java.io.IOException;
@@ -15,13 +16,8 @@ import java.util.List;
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Post post = new Post();
-        post.setTitle("ASKJDHGSADSAHDG");
-        post.setCategory(Post.CategoryType.SPORT);
+        List<Item> items = DbManager.getItems();
+        request.setAttribute("tovary", items);
         request.getRequestDispatcher("home.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     }
 }
